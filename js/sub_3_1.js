6b96b974eb731ea1a718fe12c704d4456c3c70eb
ahkimmm <aaahhkimmail@gmail.com>
2024-07-31 09:41:48 +0900

    Add files via upload

---
+++ js/sub_3_1.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 5,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+      },
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
